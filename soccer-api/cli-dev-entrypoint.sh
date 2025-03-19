71d5b3c9dda9ed26ad1398bbdb34a4136b3e1004
jtonyNet <jtony.net@gmail.com>
2025-03-19 18:39:36 -0300

    fix: import competitions dockerizada em SO windonws

--- soccer-api/cli-dev-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start the hot reload
-CompileDaemon -build="go build -o /usr/src/app/bin/cli/main /usr/src/app/cmd/cli/main.go" -command="./bin/cli/main" &
-
-# Keep the container running
-tail -f /dev/null
